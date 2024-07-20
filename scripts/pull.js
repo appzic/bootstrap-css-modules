84cc88912c9b0e64c166da54fb82548535cff771
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-07-20 16:42:07 +0530

    Update: scripts with class

--- scripts/pull.js
+++ scripts/pull.js
@@ -3,28 +3,39 @@ import { execSync } from "node:child_process";
 import axios from "axios";
 import unzipper from "unzipper";
 
-const VERSION = "1.0.0";
+class Pull {
+	constructor() {
+		this.version = this._readVersion();
+	}
 
-async function download() {
-	try {
-		const url = `https://github.com//twbs/bootstrap/archive/refs/tags/v${VERSION}.zip`;
-		const response = await axios.get(url, { responseType: "arraybuffer" });
-		const fileData = Buffer.from(response.data, "binary");
-		fs.writeFileSync(`temp/bs-${VERSION}.zip`, fileData);
-	} catch (err) {
-		console.error(err);
+	_readVersion() {
+		const jsonData = fs.readFileSync("package.json", { encoding: "utf-8" });
+		const obj = JSON.parse(jsonData);
+		return obj.version;
+	}
+
+	async download() {
+		try {
+			const url = `https://github.com//twbs/bootstrap/archive/refs/tags/v${this.version}.zip`;
+			const response = await axios.get(url, { responseType: "arraybuffer" });
+			const fileData = Buffer.from(response.data, "binary");
+			fs.writeFileSync(`temp/bs-${this.version}.zip`, fileData);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async unzip() {
+		const directory = await unzipper.Open.file(`temp/bs-${this.version}.zip`);
+		await directory.extract({ path: "temp" });
+	}
+
+	async run() {
+		execSync("mkdir -p temp");
+		await this.download();
+		await this.unzip();
 	}
 }
 
-async function unzip() {
-	const directory = await unzipper.Open.file(`temp/bs-${VERSION}.zip`);
-	await directory.extract({ path: "temp" });
-}
-
-async function main() {
-	execSync("mkdir -p temp");
-	await download();
-	await unzip();
-}
-
-main();
+const pull = new Pull();
+pull.run();
--- scripts/push.js
+++ scripts/push.js
@@ -5,57 +5,61 @@ import postcss from "postcss";
 import safeParser from "postcss-safe-parser";
 import { camelCase } from "change-case";
 
-const VERSION = "1.0.0";
-const INPUT_OUTPUT_FILES = [
-	{
-		src: `temp/bootstrap-${VERSION}/bootstrap-${VERSION}.css`,
-		dist: `dist/bootstrap-${VERSION}.module.css`,
-	},
-];
+class Push {
+	constructor() {
+		this.version = this._readVersion();
 
-function updateVersion() {
-	const jsonData = fs.readFileSync("package.json", { encoding: "utf-8" });
-	const obj = JSON.parse(jsonData);
-	obj.version = VERSION;
-	fs.writeFileSync("package.json", JSON.stringify(obj));
+		this.ioFiles = [
+			{
+				src: `temp/bootstrap-${this.version}/bootstrap-${this.version}.css`,
+				dist: `dist/bootstrap-${this.version}.module.css`,
+			},
+		];
+	}
+
+	_readVersion() {
+		const jsonData = fs.readFileSync("package.json", { encoding: "utf-8" });
+		const obj = JSON.parse(jsonData);
+		return obj.version;
+	}
+
+	convertClassNames(css) {
+		const root = postcss.parse(css, { parser: safeParser });
+
+		root.walkRules((rule) => {
+			if (rule.selector) {
+				rule.selector = rule.selector.replace(/\.([\w-]+)/g, (match, p1) => {
+					return `.${camelCase(p1)}`;
+				});
+			}
+		});
+
+		return root.toString();
+	}
+
+	mkCSSModules() {
+		this.ioFiles.forEach(({ src, dist }) => {
+			let data = fs.readFileSync(src, { encoding: "utf-8" });
+			data = this.convertClassNames(data);
+			fs.writeFileSync(dist, data);
+		});
+	}
+
+	run() {
+		execSync("mkdir -p dist");
+		mkCSSModules();
+		execSync("npm run typed-css");
+		execSync("npm run format");
+		execSync("npm i");
+		execSync("git add .");
+		execSync(`git commit -m "v${this.version}"`);
+		execSync("git push");
+		execSync(`git tag v${this.version}`);
+		execSync("git push --tag");
+		execSync("npm publish --access public");
+		rimrafSync("temp");
+	}
 }
 
-const convertClassNames = (css) => {
-	const root = postcss.parse(css, { parser: safeParser });
-
-	root.walkRules((rule) => {
-		if (rule.selector) {
-			rule.selector = rule.selector.replace(/\.([\w-]+)/g, (match, p1) => {
-				return `.${camelCase(p1)}`;
-			});
-		}
-	});
-
-	return root.toString();
-};
-
-function mkCSSModules() {
-	INPUT_OUTPUT_FILES.forEach(({ src, dist }) => {
-		let data = fs.readFileSync(src, { encoding: "utf-8" });
-		data = convertClassNames(data);
-		fs.writeFileSync(dist, data);
-	});
-}
-
-async function main() {
-	execSync("mkdir -p dist");
-	updateVersion();
-	execSync("npm i");
-	mkCSSModules();
-	execSync("npm run typed-css");
-	execSync("npm run format");
-	execSync("git add .");
-	execSync(`git commit -m "v${VERSION}"`);
-	execSync("git push");
-	execSync(`git tag v${VERSION}`);
-	execSync("git push --tag");
-	execSync("npm publish --access public");
-	rimrafSync("temp");
-}
-
-main();
+const push = Push();
+push.run();
